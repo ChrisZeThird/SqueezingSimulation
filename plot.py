07e78abb4939229583b104bb7744392cc328ddd9
Chris Ze Third <wilantchri.game@gmail.com>
2024-05-07 11:28:06 +0200

    Can now tweak matplotlib parameters for bigger font size and LaTex like font in plot

--- plot.py
+++ plot.py
@@ -1,3 +1,4 @@
+import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import numpy as np
@@ -5,6 +6,24 @@ import numpy as np
 from utils.misc import wavelength_to_omega
 from noise_spectrum import noise_spectrum_x, noise_spectrum_p
 from utils.settings import settings
+
+# -- Matplotlib parameters -- #
+SMALL_SIZE = 12
+MEDIUM_SIZE = 16
+BIGGER_SIZE = 22
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
+# print(sorted([f.name for f in matplotlib.font_manager.fontManager.ttflist]))
 
 # -- Arrays definitions -- #
 lambda_array = np.linspace(start=0, stop=1000, num=settings.array_points) * 1e-9  # wavelength
@@ -32,7 +51,7 @@ for line in legend_quadrature.get_lines():
 
 plt.xlabel("$P/P_{thr} = \epsilon^2$")
 plt.ylabel('$S$ (dB)')
-plt.title('Squeezing and anti-squeezing versus pump power at zero frequency')
+# plt.title('Squeezing and anti-squeezing versus pump power at zero frequency')
 
 # Adjust the layout to accommodate both legends
 plt.subplots_adjust(right=0.85)
@@ -41,8 +60,10 @@ plt.show()
 
 # -- Squeezing and anti-squeezing versus wavelength -- #
 fig2 = plt.figure(figsize=(16, 9))
-sx = noise_spectrum_x(omega=omega_array, omega_c=settings.omega_c, escape_efficiency=0.8, epsilon=0.5)
-sp = noise_spectrum_p(omega=omega_array, omega_c=settings.omega_c, escape_efficiency=0.8, epsilon=0.5)
+escape_efficiency = 0.8
+epsilon = 0.1
+sx = noise_spectrum_x(omega=omega_array, omega_c=settings.omega_c, escape_efficiency=escape_efficiency, epsilon=epsilon)
+sp = noise_spectrum_p(omega=omega_array, omega_c=settings.omega_c, escape_efficiency=escape_efficiency, epsilon=epsilon)
 plt.plot(omega_array/settings.omega_c, 10*np.log10(sx))
 plt.plot(omega_array/settings.omega_c, 10*np.log10(sp), linestyle='--')
 
@@ -51,7 +72,7 @@ legend_quadrature = plt.legend(['$s_x$', '$s_p$'], loc='upper right')
 
 plt.xlabel("$\omega/\omega_c$")
 plt.ylabel('$S$ (dB)')
-plt.title('Squeezing and anti-squeezing versus frequency ($\eta = 0.8$ and $\epsilon = 0.5$)')
+# plt.title(f'Squeezing and anti-squeezing versus noise frequency ($\eta =$ {escape_efficiency} and $\epsilon =$ {epsilon})')
 
 # Adjust the layout to accommodate both legends
 plt.subplots_adjust(right=0.85)
--- utils/misc.py
+++ utils/misc.py
@@ -3,5 +3,4 @@ from utils.settings import settings
 
 
 def wavelength_to_omega(wavelength):
-    print(settings.c)
     return 2 * pi * settings.c / wavelength
