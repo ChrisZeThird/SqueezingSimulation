1aaf16a54fabed0947d0c03f37083fdaaa48a53e
Chris Ze Third <wilantchri.game@gmail.com>
2024-05-23 10:33:04 +0200

    more explicit prints

--- cavity/cavity.py
+++ cavity/cavity.py
@@ -1,7 +1,16 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-""" Finding cavity distances """
+
+# Finding cavity distances
+def cos_theta_function(R, d_curved, l):
+    """
+
+    :param R:
+    :param d_curved:
+    :param l:
+    :return:
+    """
+    return 2 * (R/(d_curved - l))**2 - 1
 
 
 def finding_unknown_distance(L, R, l, d_curved):
@@ -18,40 +27,57 @@ def finding_unknown_distance(L, R, l, d_curved):
             'Both distances between flat mirrors and curved mirrors cannot be unknown. Please fix the distance between the curved mirror')
 
     else:
-        cos_theta = 2 * (R/(d_curved - l))**2 - 1
+        cos_theta = cos_theta_function(R, d_curved, l)
         d_flat = L/(1 + (1/cos_theta)) - d_curved
         OF = d_flat / (2 * cos_theta)
         OC = d_curved / (2 * cos_theta)
         S = (d_curved - l)/2 + OC + OF
-        print('Calculated total length L: ', d_flat + d_curved + 2 * (OC + OF))
-        print('Input total length L: ', L)
-        print('--------------------------')
-        print('Calculated equivalent cavity length S: ', S)
+        # print('Calculated total length L: ', d_flat + d_curved + 2 * (OC + OF))
+        # print('Input total length L: ', L)
+        # print('--------------------------')
+        # print('Calculated equivalent cavity length S: ', S)
         return d_flat, OF, OC, cos_theta, S
 
 
-L = 64e-2
-d_curved = 11.6e-2
-R = 10e-2
-l = 10e-3
+# L = 64e-2
+# d_curved = 11.6e-2
+# R = 10e-2
+# l = 10e-3
+#
+# d_flat, OF, OC, cos_theta, S = finding_unknown_distance(L, R, l, d_curved)
+# print('Distance between the flat mirrors: ', d_flat)
 
-d_flat, OF, OC, cos_theta, S = finding_unknown_distance(L, R, l, d_curved)
-print('Distance between the flat mirrors: ', d_flat)
+
+def equivalent_cavity(d_flat, d_curved, l, R):
+    """
+
+    :param d_flat:
+    :param d_curved:
+    :param l:
+    :param R:
+    :return:
+    """
+    cos_theta = cos_theta_function(R, d_curved, l)
+    return d_flat / (2 * cos_theta) + d_curved / (2 * cos_theta) + (d_curved - l) / 2
 
 
-def crystal_waist(L, R, S, wavelength=780e-9):
+def crystal_waist(L, R, d_curved, l, wavelength=780e-9):
     """
     Compute the beam waist on the crystal according to O. Pinel. 'Optique quantique multimode avec des peignes de
     fréquence' PhD thesis, 2010.
-    :param L: stability length
+    :param L: total cavity length
     :param R: curved mirror radius
-    :param S: length of the equivalent cavity
+    :param d_curved: distance between curved mirrors
+    :param l: crystal length
     :param wavelength:
     :return:
     """
-    A = (L - R / 2)
-    B = ((S - L) * L - S * R / 2)
-    C = (S - L - R / 2)
+    d_flat, _, _, _, S = finding_unknown_distance(L, R, l, d_curved)
+    # S = equivalent_cavity(d_flat, d_curved, l, R)
+    L_prime = (d_curved - l)/2
+    A = (L_prime - R / 2)
+    B = ((S - L_prime) * L_prime - S * R / 2)
+    C = (S - L_prime - R / 2)
     return np.sqrt(wavelength / np.pi) * ((- A * B) / C) ** (1 / 4)
 
 
@@ -60,5 +86,6 @@ radii = np.array([25, 50, 100]) * 1e-3
 crystal_lengths = np.arange(start=5, stop=30, step=5) * 1e-3
 expected_waists = {"G. Hétet": 40e-6, "Appel": None, "T. Takahito": 20e-6, "Takao": 17e-6}
 
-distance_crystal_mirror = (d_curved - l)/2
-print('Crystal waist: ', crystal_waist(L=distance_crystal_mirror, R=R, S=S, wavelength=795e-9))
+# distance_crystal_mirror = (d_curved - l)/2
+# print('Crystal waist: ', crystal_waist(L=distance_crystal_mirror, R=R, S=S, wavelength=795e-9))
+
