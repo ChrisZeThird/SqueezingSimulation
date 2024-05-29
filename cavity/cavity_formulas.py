2b0d7fb727544142ba6e3a252e45b80646d07945
Chris Ze Third <wilantchri.game@gmail.com>
2024-05-29 13:49:15 +0200

    Now works just fine!

--- cavity/cavity_formulas.py
+++ cavity/cavity_formulas.py
@@ -57,116 +57,59 @@ def Pump_power(T, Loss, E):
 
 
 # -- Ray propagation -- #
-def ABCD_Matrix(d_curved, d_flat, d_diag, R, l_crystal, index_crystal=1):
+def ABCD_Matrix(L, d_curved, R, l_crystal, index_crystal=1):
     """
+    Ray transfer matrix for a single pass in a ring bow-tie cavity.
 
+    :param L: Cavity round-trip length
     :param d_curved: Distance between curved mirrors
-    :param d_flat: Distance between flat mirrors
-    :param d_diag: Distance between curved and flat mirrors (diagonal line)
     :param R: Radii of curvature of curved mirror
     :param l_crystal: Length of non-linear crystal
     :param index_crystal: Index of refraction of non-linear medium (by default 1)
     :return: Tuple (A, B, C, D)
     """
-    n = np.size(d_curved)
-    one = np.ones(n)
-    zero = np.zeros(n)
 
-    # set lengths and focals
-    f = R / 2
-    free_propagation_flat = d_flat + 2 * d_diag
-    free_propagation_curved = (d_curved - l_crystal) / 2
-    free_propagation_crystal = l_crystal / index_crystal
+    E = (L - d_curved) / 2
 
-    # M1 = np.array([[one, d_diag + d_flat / 2], [zero, one]])
-    # M2 = np.array([[one, zero], [-2 / R * one, one]])
-    # M3 = np.array([[one, (d_curved - l_crystal) / 2], [zero, one]])
-    # Unity = np.array([[one, zero], [zero, one]])  # air to crystal and crystal to air
-    # M4 = np.array([[one, l_crystal / index_crystal * one], [zero, one]])
+    A1 = 1 - (2 / R) * E
+    B1 = A1 * ((l_crystal/(2 * index_crystal)) + (d_curved - l_crystal) / 2) + E
+    C1 = - 2 / R
+    D1 = (1 / index_crystal) * (C1 * (d_curved - l_crystal) / 2 + 1)
 
-    M1 = np.array([[one, zero], [- 1 / (2 * f) * one, one]])
-    M2 = np.array([[one, free_propagation_flat], [zero, one]])
-    M3 = np.array([[one, zero], [- 1 / f * one, one]])
-    M4 = np.array([[one, free_propagation_curved], [zero, one]])
-    M5 = np.array([[one, free_propagation_crystal * one], [zero, one]])
-
-    # print(M1.shape, M2.shape, M3.shape, M4.shape, M5.shape)
-
-    axis = 1
-    ABCD = np.empty((2, 2, n))
-    for i in range(n):
-        # ABCD[:, :, i] = np.matmul(np.matmul(np.matmul(np.matmul(M1[:, :, i], M2[:, :, i]), M3[:, :, i]), M4[:, :, i]), M5[:, :, i])
-        ABCD[:, :, i] = M1[:, :, i] @ M2[:, :, i] @ M3[:, :, i]  @ M4[:, :, i] @ M5[:, :, i] @ M4[:, :, i] @  M3[:, :, i]
-
-    # E = (l_crystal / (2 * index_crystal)) + ((d_curved - l_crystal) / 2) * (1 / index_crystal)
-    # F = ((- 2 / R) * E) + (1 / index_crystal)
-    # # F = ((- 2 / R) * E)
-    #
-    # A = 1 + (d_diag + (d_flat / 2)) * (- 2 / R)
-    # B = E + (d_diag + (d_flat / 2)) * F
-    # # B = ((d_diag + (d_flat / 2)) + 1) * E
-    # C = - 2 / R
-    # D = F
-
-    A, B, C, D = ABCD[0, 0, :], ABCD[0, 1, :], ABCD[1, 0, :], ABCD[1, 1, :]
-    # print(A*B - D*C)
-    # print(A.shape, B.shape, C.shape, D.shape)
-
-    return A, B, C, D
+    return A1, B1, C1, D1
 
 
 # -- Tamagawa -- #
-def Rayleigh_length(A, B, C, D):
-    """
-    Calculate Rayleigh length from ABCD matrix elements of beam waist at the crystal center
-    :param A:
-    :param B:
-    :param C:
-    :param D:
-    :return: tuple with the Rayleigh length and the corresponding indices with allowed values
-    """
-    # print(- (A * B) / (C * D) > 0)
-    product = - (A * B) / (C * D)
-    temp = np.full(shape=A.shape, fill_value=np.nan, dtype=np.float32)
-
-    valid_indices = np.where(product >= 0)  # ensures the square root is taken for positive terms only
-    temp[valid_indices] = np.sqrt(product[valid_indices])
-    return temp[valid_indices], valid_indices
-
-
-def Beam_waist(d_curved, L, cavity_width, R, l_crystal, index_crystal=1, wavelength=780e-9, tamagawa=False):
+def Beam_waist(d_curved, L, R, l_crystal, index_crystal=1, wavelength=780e-9):
     """
     Calculates the beam waist size in radius at the center of the nonlinear optical crystal and the intermediate
     between flat mirrors
     :param d_curved: Distance between curved mirrors
     :param L: Cavity length
-    :param cavity_width:
     :param R: Radii of curvature of curved mirror
     :param l_crystal: Length of non-linear crystal
     :param index_crystal: Index of refraction of non-linear medium (by default 1)
     :param wavelength:
-    :param tamagawa: Boolean, to calculate missing distances with the formula from Tamagawa Vol.2-3
     :return: Tuple (w1, w2, valid_indices)
     """
-    if tamagawa:
-        d_diag = finding_diagonal_tamagawa(L=L, cavity_width=cavity_width)
-        d_flat = finding_flat_tamagawa(L=L, cavity_width=cavity_width, d_curved=d_curved)
-        # print('d_diag: ', d_diag)
-        # print('d_flat: ', d_flat)
-        # print('-----------------------------')
-    else:
-        d_flat, OF, OC, _, _ = fd.finding_unknown_distance(L=L, R=R, l=l_crystal, d_curved=d_curved)
-        d_diag = OF + OC
+    A1, B1, C1, D1 = ABCD_Matrix(L=L, d_curved=d_curved, R=R, l_crystal=l_crystal, index_crystal=index_crystal)
 
-    A, B, C, D = ABCD_Matrix(d_curved, d_flat, d_diag, R, l_crystal, index_crystal=index_crystal)
-    # print(np.shape(A), np.shape(B), type(C), np.shape(D))
-    rayleigh_length, valid_indices = Rayleigh_length(A, B, C, D)
+    q1 = - (B1 * D1) / (A1 * C1)  # wavefront at mirror 1
+    # print('q1: ', q1)
+    temp1 = np.full(shape=q1.shape, fill_value=np.nan, dtype=np.float32)
+    valid_indices_1 = np.where(q1 >= 0)  # ensures the square root is taken for positive terms only
+    # print('valid_indices_1: ', valid_indices_1)
+    temp1[valid_indices_1] = np.sqrt(q1[valid_indices_1])
+    w1 = (temp1 ** (1/4)) * ((wavelength / (index_crystal * np.pi)) ** (1 / 2))
 
-    w1 = np.sqrt((wavelength / np.pi) * rayleigh_length)
-    w2 = np.sqrt((A[valid_indices]) ** 2 + (B[valid_indices] / rayleigh_length)**2) * w1 * index_crystal
-    # w2 = (1 / (np.sqrt((C * rayleigh_length) ** 2 + D[valid_indices]))) * w1 / index_crystal
+    q2 = - (B1 * A1) / (D1 * C1)  # wavefront at mirror 2
+    temp2 = np.full(shape=q2.shape, fill_value=np.nan, dtype=np.float32)
+    valid_indices_2 = np.where(q2 >= 0)  # ensures the square root is taken for positive terms only
+    # print('valid_indices_2: ', valid_indices_2)
+    temp2[valid_indices_2] = np.sqrt(q2[valid_indices_2])
+    w2 = (temp2 ** (1 / 4)) * ((wavelength / (index_crystal * np.pi)) ** (1 / 2))
 
-    return w1, w2, valid_indices
+    return w1, w2, valid_indices_1, valid_indices_2
 
 
 # -- Missing lengths -- #
@@ -237,14 +180,3 @@ def waist_mirror3(R1, R2, L, wavelength):
 
 def waist_intracavity(R1, R2, L, wavelength):
     return ((wavelength / np.pi) ** 2 * (L * (R1 - L) * (R2 - L) * (R1 + R2 - L) / ((R1 + R2 - 2 * L) ** 2))) ** (1 / 4)
-
-
-# -- Loosing my mind over this -- #
-def brute_force(d_curved, l_crystal, index_crystal):
-    """
-
-    :param d_curved:
-    :param l_crystal:
-    :param index_crystal:
-    :return:
-    """
