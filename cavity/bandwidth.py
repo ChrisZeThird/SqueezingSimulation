30ef69a6a5ad504277349952ea0dee1385643931
Chris Ze Third <wilantchri.game@gmail.com>
2024-06-06 14:55:31 +0200

    Plot multi bandwidth regions on the same plot, axes are properly shown. Works for 3 regions for the moment.

--- cavity/bandwidth.py
+++ cavity/bandwidth.py
@@ -22,32 +22,28 @@ def bandwidth():
     # Bandwidths list
     bandwidth_list = np.array([1, 10, 100]) * 1e6
     bandwidth_colours = ['lightsteelblue', 'cornflowerblue', 'royalblue']
-    bandwidth_cmaps = [mplp.symmetrical_colormap((x, None)) for x in ['Blues', 'Greens', 'Reds']]
+    # bandwidth_cmaps = [mplp.symmetrical_colormap((x, None)) for x in ['Blues', 'Greens', 'Reds']]
+    bandwidth_cmaps = ['Blues', 'Greens', 'Reds']
 
     # Bandwidth surface plot
     clev = np.arange(bandwidth_meshgrid.min(), bandwidth_meshgrid.max(), 0.2)
 
-    fig_bandwidth = plt.figure(figsize=(12, 6))
-    ax1 = plt.subplot(3, 2, 6)
-    ax2 = plt.subplot(3, 2, 4)
-    ax3 = plt.subplot(3, 2, 2)
-    ax_bandwidth = plt.subplot(1, 3, 1)
+    fig_bandwidth = plt.figure(figsize=(16, 9))
+    # ax1 = plt.subplot(3, 2, 6)
+    # ax2 = plt.subplot(3, 2, 4)
+    # ax3 = plt.subplot(3, 2, 2)
+    # Creating three axes: add_axes([xmin,ymin,dx,dy])
+    ax1 = fig_bandwidth.add_axes((0.75, 0.1, 0.1, 0.28))
+    ax2 = fig_bandwidth.add_axes((0.75, 0.395, 0.1, 0.28))
+    ax3 = fig_bandwidth.add_axes((0.75, 0.69, 0.1, 0.28))
+    ax_bandwidth = fig_bandwidth.add_axes((0.1, 0.1, 0.7, 0.87))
+    # ax_title_cbar = fig_bandwidth.add_axes((0.85, 0.05, 0.1, 0.92))
     axes = [ax1, ax2, ax3]
 
     # contour_plot = ax_bandwidth.contourf(L, T, bandwidth_meshgrid, clev, cmap=mplp.cmap, norm=matplotlib.colors.LogNorm(vmin=1, vmax=bandwidth_meshgrid.max()))
     # contour_plot.set_clim(vmin=1, vmax=round(bandwidth_meshgrid.max()))
     # cbar = plt.colorbar(contour_plot, ax=ax_bandwidth)
     # cbar.outline.set_visible(False)
-
-    # Adjust ticks for colorbar
-    nb_ticks = 7
-    xticks_bandwidth = np.around(np.linspace(start=settings.min_L, stop=settings.max_L, num=nb_ticks), decimals=1)
-    yticks_bandwidth = np.around(np.linspace(start=settings.min_T, stop=settings.max_T, num=nb_ticks), decimals=1)
-    new_xticks_bandwidth = np.linspace(0, settings.number_points, nb_ticks)
-    new_yticks_bandwidth = np.linspace(0, settings.number_points, nb_ticks)
-
-    # Prepare the custom colours to update the cbar with
-    # cbar_newcolors = []
 
     for central_freq, colour, selected_cmap, axis in zip(bandwidth_list, bandwidth_colours, bandwidth_cmaps, axes):
 
@@ -68,25 +64,21 @@ def bandwidth():
         mask[indices] = False
         masked_bandwidth = np.ma.masked_array(bandwidth_meshgrid, mask)
 
-        # cbar.ax.axhline(y=central_freq*1e-6, color=colour, linewidth=2, alpha=settings.alpha)
-        # line_text = cbar.ax.text(central_freq*1e-6, 1, f'{central_freq*1e-6}', ha='left', va='center')
-        # cbar.ax.axhline(y=boundary_down, color=colour, linestyle='--', linewidth=2)
-        # cbar.ax.axhline(y=boundary_up, color=colour, linestyle='--', linewidth=2)
-
         # Plot the masked region with a different colormap
         contour_zone = ax_bandwidth.contourf(L, T, masked_bandwidth, cmap=selected_cmap, alpha=settings.alpha)
         axis.axis('off')
-        plt.colorbar(contour_zone, ax=axis)
-
-        # Plot the boundaries for the region
-        # ax_bandwidth.plot(L[indices], T[indices], c=colour, alpha=settings.alpha, label=f'{central_freq * 1e-6}MHz')
-
-        # ax_bandwidth.plot(length_range, transmission_range, c=colour, alpha=settings.alpha, label=f'{central_freq*1e-6}MHz')
-        # ax_bandwidth.legend(labelcolor='linecolor')
+        cbar_zone = plt.colorbar(contour_zone, ax=axis)
+        cbar_zone_ticks = cbar_zone.get_ticks()
+        cbar_zone.set_ticks(cbar_zone_ticks[1:-1])
 
     ax_bandwidth.set_xlabel('Cavity length L (m)')
     ax_bandwidth.set_ylabel('Transmission coefficient')
-    # cbar.set_label(r'Bandwidth $\Delta$ (MHz) in log scale')
-    # cbar.set_ticks([])
-    plt.tight_layout()
+
+    # Add text vertically on the right side of the axes
+    text_y_pos = 0.5  # Vertically centered position
+    text_x_pos = 0.92  # Right side position
+    text_vertical = "Bandwidth Δ (MHz)"
+    plt.text(text_x_pos, text_y_pos, text_vertical, horizontalalignment='center', verticalalignment='center',
+             rotation=270, transform=fig_bandwidth.transFigure, fontsize=mplp.MEDIUM_SIZE)
+
     plt.show()
