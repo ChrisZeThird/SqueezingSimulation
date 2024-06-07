1ef52cec15deacc1a585e1f7c3f6ec7b0d56c5c0
Chris Ze Third <wilantchri.game@gmail.com>
2024-06-07 14:07:51 +0200

    check non-linear coefficient depending on (T,L) or (T+L, P_th)

--- squeezing/threshold.py
+++ squeezing/threshold.py
@@ -12,51 +12,31 @@ from utils.settings import settings
 
 def slider_threshold():
     T = np.linspace(start=0, stop=0.2, num=settings.number_points)
-    L = np.linspace(start=0, stop=0.1, num=settings.number_points)
+    L = np.linspace(start=0, stop=0.05, num=settings.number_points)
 
-    X = np.linspace(start=0, stop=0.9, num=settings.number_points)
-    P = np.linspace(start=0, stop=250, num=settings.number_points) * 1e-3
+    X = np.linspace(start=0., stop=0.12, num=settings.number_points)
+    P = np.linspace(start=100, stop=250, num=settings.number_points) * 1e-3
 
     TT, LL = np.meshgrid(T, L)
-
     XX, PP = np.meshgrid(X, P)
 
-    # Initial values
-    init_T = 0.2
-    init_L = 0.0
-    init_P = 150e-3  # 150 mW in Watts
-
     # Calculate the initial values for E_NL
-    # E_NL = ((TT + LL) ** 2) * init_P / 4
-    E_NL = (XX ** 2) * PP / 4
+    E_NL = ((TT + LL) ** 2) * 150e-3 / 4
+    # E_NL = (XX ** 2) / (PP * 4)
 
     # Create the figure and the line that we will manipulate
-    fig = plt.figure()
+    fig = plt.figure(figsize=(9, 9))
 
     ax1 = fig.add_subplot(111, projection='3d')
     # ax2 = fig.add_axes((0.1, 0.85, 0.8, 0.1))
-    ax1.plot_surface(XX, PP, E_NL, cmap='viridis')
+    # ax1.plot_surface(XX, PP, E_NL, cmap='viridis')
+    ax1.plot_surface(TT, LL, E_NL, cmap='viridis')
     # Plot the 3D surface
-    ax1.set_xlabel('Transmission coefficient (T)', fontsize=18)
-    ax1.set_ylabel('Pump threshold power', fontsize=18)
-    ax1.set_zlabel('Effective Non-linearity (E_NL)', fontsize=18)
-    ax1.set_title('3D Surface Plot')
+    ax1.set_xlabel('\nTransmission coefficient (T)', fontsize=18)
+    # ax1.set_ylabel('\nPump threshold power', fontsize=18)
+    ax1.set_ylabel('\nIntracavity loss', fontsize=18)
+    ax1.set_zlabel('\nEffective Non-linearity $E_{NL}$ (W$^{-1}$)', fontsize=18, labelpad=20)
+    ax1.tick_params(axis='z', which='major', pad=15)
 
-    # Add a slider for P_thr
-    # s = Slider(ax=ax2, label='P_thr (mW)', valmin=0, valmax=250, valinit=init_P * 1e3)
-    #
-    # # Update function to be called anytime the slider's value changes
-    # def update(val):
-    #     P_thr = s.val * 1e-3  # Convert from W to mW
-    #     E_NL = ((TT + LL) ** 2) * P_thr / 4
-    #     ax1.cla()
-    #     ax1.plot_surface(T, L, E_NL, cmap='viridis')
-    #     # ax.set_xlabel('Transmission coefficient (T)')
-    #     # ax.set_ylabel('Loss (L)')
-    #     # ax.set_zlabel('Effective Non-linearity (E_NL)')
-    #     # ax.set_title('3D Surface Plot')
-    #
-    # # Call update function when slider value changes
-    # s.on_changed(update)
-
+    plt.tight_layout()
     plt.show()
