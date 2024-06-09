21efdb5c8b4eba5f074026e1a9f7482a06c97fea
Chris Ze Third <wilantchri.game@gmail.com>
2024-06-09 11:03:54 +0200

    Better plot of  non-linear coefficient depending on (T,L) and (T+L, P_th)

--- squeezing/threshold.py
+++ squeezing/threshold.py
@@ -21,22 +21,32 @@ def slider_threshold():
     XX, PP = np.meshgrid(X, P)
 
     # Calculate the initial values for E_NL
-    E_NL = ((TT + LL) ** 2) * 150e-3 / 4
-    # E_NL = (XX ** 2) / (PP * 4)
+    fixed_P = 150e-3
+    E_NL_fixedP = ((TT + LL) ** 2) * fixed_P / 4
+    E_NL = (XX ** 2) / (PP * 4)
 
     # Create the figure and the line that we will manipulate
-    fig = plt.figure(figsize=(9, 9))
+    # E vs (T,L)
+    fig1 = plt.figure(figsize=(9, 9))
+    ax1 = fig1.add_subplot(111, projection='3d')
 
-    ax1 = fig.add_subplot(111, projection='3d')
-    # ax2 = fig.add_axes((0.1, 0.85, 0.8, 0.1))
-    # ax1.plot_surface(XX, PP, E_NL, cmap='viridis')
-    ax1.plot_surface(TT, LL, E_NL, cmap='viridis')
-    # Plot the 3D surface
     ax1.set_xlabel('\nTransmission coefficient (T)', fontsize=18)
-    # ax1.set_ylabel('\nPump threshold power', fontsize=18)
     ax1.set_ylabel('\nIntracavity loss', fontsize=18)
     ax1.set_zlabel('\nEffective Non-linearity $E_{NL}$ (W$^{-1}$)', fontsize=18, labelpad=20)
     ax1.tick_params(axis='z', which='major', pad=15)
+
+    ax1.plot_surface(TT, LL, E_NL_fixedP, cmap='viridis')
+
+    # E vs (T+L, P)
+    fig2 = plt.figure(figsize=(9, 9))
+    ax2 = fig2.add_subplot(111, projection='3d')
+
+    ax2.set_xlabel('\nTransmission coefficient (T)', fontsize=18)
+    ax2.set_ylabel('\nPump threshold power', fontsize=18)
+    ax2.set_zlabel('\nEffective Non-linearity $E_{NL}$ (W$^{-1}$)', fontsize=18, labelpad=20)
+    ax2.tick_params(axis='z', which='major', pad=15)
+
+    ax2.plot_surface(XX, PP, E_NL, cmap='viridis')
 
     plt.tight_layout()
     plt.show()
