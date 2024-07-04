fbf7ce5ef074ef7cea22eccc64fccc773bf50ae9
Chris Ze Third <wilantchri.game@gmail.com>
2024-07-04 13:01:47 +0200

    Started working on better GUI. First step: make a frame with an embedded figure.

---
+++ gui/plot_embedder.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class PlotEmbedder:
+    def __init__(self, parent, title):
+        self.frame = ttk.Frame(parent)
+        self.frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        self.figure, self.ax = plt.subplots()
+        self.ax.set_title(title)
+
+        self.canvas = FigureCanvasTkAgg(self.figure, self.frame)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def update_plot(self, x, y):
+        self.ax.clear()
+        self.ax.plot(x, y)
+        self.canvas.draw()
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from tkinter import Tk, mainloop
+
 from cavity.bandwidth import bandwidth
 from cavity.waist import waist, Kaertner
 
@@ -6,25 +8,31 @@ from squeezing.threshold import slider_threshold
 
 from utils.settings import settings
 
+from gui.plot_embedder import PlotEmbedder
+
 if __name__ == '__main__':
-    # -- Squeezing -- #
-    if settings.plot_pump_power:
-        squeezing_vs_pump()
+    # # -- Squeezing -- #
+    # if settings.plot_pump_power:
+    #     squeezing_vs_pump()
+    #
+    # if settings.squeezing_wavelength:
+    #     squeezing_vs_wavelength()
+    #
+    # if settings.plot_threshold:
+    #     slider_threshold()
+    #
+    # # -- Optimize bandwidth -- #
+    # if settings.plot_bandwidth:
+    #     bandwidth()
+    #
+    # # -- Optimize waist -- #
+    # if settings.plot_waist:
+    #     waist()
+    #
+    # # -- Kaertner class notes -- #
+    # if settings.plot_kaertner:
+    #     Kaertner()
 
-    if settings.squeezing_wavelength:
-        squeezing_vs_wavelength()
-
-    if settings.plot_threshold:
-        slider_threshold()
-
-    # -- Optimize bandwidth -- #
-    if settings.plot_bandwidth:
-        bandwidth()
-
-    # -- Optimize waist -- #
-    if settings.plot_waist:
-        waist()
-
-    # -- Kaertner class notes -- #
-    if settings.plot_kaertner:
-        Kaertner()
+    root = Tk()
+    pe = PlotEmbedder(parent=root, title="Test Plot")
+    mainloop()
