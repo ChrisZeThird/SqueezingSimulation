7fdd32c5cc8264414a116d5d2b0e0d0d845b3343
Chris Ze Third <wilantchri.game@gmail.com>
2025-04-30 16:45:39 +0200

    Added poling period calculation in nlo/sellmeier folder

--- nlo/sellmeier.py
+++ nlo/sellmeier.py
@@ -20,3 +20,23 @@ def n_z(lambda_um):
     square = A + B / (1 - (C / lambda_um)**2) - D * lambda_um**2
 
     return sqrt(square)
+
+
+def poling_period(lambda_fund_um):
+    """
+    Compute the poling period l_poling (in micrometers) for quasi-phase matching in KTP.
+
+    Parameters:
+        lambda_fund_um (float): Fundamental wavelength (e.g., 1.56 for 1560 nm)
+
+    Returns:
+        float: Poling period in micrometers
+    """
+    lambda_sh_um = lambda_fund_um / 2  # SHG wavelength
+    n_fund = n_z(lambda_fund_um)
+    n_sh = n_z(lambda_sh_um)
+
+    delta_n = n_sh - n_fund
+    l_poling = lambda_sh_um / delta_n  # formula derived above
+
+    return l_poling
